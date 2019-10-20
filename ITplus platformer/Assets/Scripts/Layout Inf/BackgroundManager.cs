c8d413156d2d61f61875b971241efe99429bdc70
minh hoang nguyen <nhminh0701@gmail.com>
2019-10-20 18:04:29 +0700

    Level Design (6/10)

---
+++ ITplus platformer/Assets/Scripts/Layout Inf/BackgroundManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+
+    [SerializeField] Transform background1;
+    [SerializeField] Transform background2;
+
+    [SerializeField] Transform thisBG;
+    [SerializeField] Transform newBG;
+    Transform tempPos;
+    Transform myCamera;
+    float distance;
+
+    private void Start()
+    {
+        myCamera = Camera.main.transform;
+        SetBG();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBG();
+    }
+
+    private void MoveBG()
+    {
+        distance = myCamera.position.x - thisBG.position.x;
+
+        if (Mathf.Abs(distance) > 4)
+        {
+            Vector3 thisBGPos = thisBG.position;
+            newBG.position = thisBGPos + new Vector3(23, 0, 0) * Math.Sign(distance);
+
+            SwapBG();
+        }
+    }
+
+    private void SwapBG()
+    {
+        tempPos = thisBG;
+        thisBG = newBG;
+        newBG = tempPos;
+    }
+
+    private void SetBG()
+    {
+        if (background1.position.x - myCamera.position.x < background1.position.y - myCamera.position.x)
+        {
+            thisBG = background1;
+            newBG = background2;
+        } else
+        {
+            thisBG = background2;
+            newBG = background1;
+        }
+
+        distance = myCamera.position.x - thisBG.position.x;
+
+        if (Mathf.Abs(distance) > 5)
+        {
+            thisBG.position = new Vector3(myCamera.position.x, myCamera.position.y, thisBG.position.z);
+        }
+    }
+}
---
+++ ITplus platformer/Assets/Scripts/Layout Inf/ReadBlockInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class ReadBlockInfo : MonoBehaviour
+{
+    [Tooltip("Name of the file you want to save/overwrite")]
+    [SerializeField] string levelName = "Level 1";
+
+    Dictionary<int, BlockInfo> blockInforList = new Dictionary<int, BlockInfo>();
+
+    public void ReadEnvironmentInfo()
+    {
+        ElementInfo[] currentBlocks = FindObjectsOfType<ElementInfo>();
+
+        for (var index = 0; index < currentBlocks.Length; index ++)
+        {
+            BlockInfo newBlock = new BlockInfo(currentBlocks[index].GetBlockID(), currentBlocks[index].transform.position.x, currentBlocks[index].transform.position.y);
+            Debug.Log(newBlock.position.x + " " + currentBlocks[index].transform.position.y);
+            blockInforList.Add(index, newBlock);
+        }
+
+        string destination = Path.Combine(Application.persistentDataPath, levelName + ".txt");
+        Debug.Log(JsonConvert.SerializeObject(blockInforList));
+        File.WriteAllText(destination, JsonConvert.SerializeObject(blockInforList));
+        Debug.Log(destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class BlockInfo
+{
+    public int id;
+    public Vector2 position;
+
+    public BlockInfo(int _id, float _xPos, float _yPos)
+    {
+        id = _id;
+        position = new Vector2(_xPos, _yPos);
+    }
+
+
+}
+
